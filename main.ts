f4bcf9d8ad1c0bbc674d8de5a04a9c2cd04e528f
Patrick Coxall <mr.coxall@mths.ca>
2023-09-01 13:02:08 -0400

    typescript version

--- main.ts
+++ main.ts
@@ -2,7 +2,22 @@
  *
  * Created by: Mr. Coxall
  * Created on: Sep 2020
- * This program ...
+ * This program shows current temperature, in Kelvin.
 */
 
-basic.showString('Hello, World!')
+// variables
+let currentTemperatureCelcius = 0
+let currentTemperatureKelvin = 0
+
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+input.onButtonPressed(Button.A, function () {
+  basic.clearScreen()
+  currentTemperatureCelcius = input.temperature()
+  currentTemperatureKelvin = currentTemperatureCelcius + 273.15
+  
+  basic.showString('The temperature is: ' + currentTemperatureKelvin.toString() + ' K.')
+  basic.clearScreen()
+  basic.showIcon(IconNames.Happy)
+})
