5dbf88ada2f2862199deae3ad89e4a502931595e
Patrick Coxall <mr.coxall@mths.ca>
2023-09-13 19:25:02 +0000

    python fix

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -18,7 +18,8 @@ while True:
     if button_a.is_pressed():
         current_temperature_celcius = temperature()
         current_temperature_kelvin = current_temperature_celcius + 273.15
-        display.scroll("The temperature is " + str(current_temperature_kelvin) + " K.")
+        current_temperature_kelvin = round(current_temperature_kelvin)
+        display.scroll("The temperature is: " + str(current_temperature_kelvin) + " K.")
 
         display.clear()
         display.show(Image.HAPPY)
